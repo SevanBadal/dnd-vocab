d09b52b070d5f4a10d04a553efc013d500f7055d
sevan golnazarian <sgolnaza@gmail.com>
2020-09-21 23:37:12 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import './App.css';
+import data_csv from './clean_data.csv';
+import WordCard from './components/word_card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+
+const d3 = require('d3');
+
+
+function App() {
+
+  const [words, setWords] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  
+  const handleQuery = event => {
+    console.log(event.target.value)
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect( () => {
+    const asyncLoadWords = async () => {
+      const dnd_words = await d3.csv(data_csv);
+      setWords(dnd_words);
+    }
+    asyncLoadWords();    
+  },[]);
+
+  const results = useMemo(() => {
+    return words.filter(wordObject => {
+      return wordObject.原词.toLowerCase().includes(searchTerm);
+    });
+  }, [words, searchTerm]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>DnD 5E 词表卡</h2>
+        <div>
+      
+          <a href="https://github.com/">  <FontAwesomeIcon icon={faGithub} /></a>
+        </div>
+        <div className="Word-container">
+          <input 
+            className="input"
+            type="text" 
+            placeholder="Tiefling" 
+            value={searchTerm}
+            onChange={handleQuery} 
+            />
+          {results.map(wordObject => (
+            <WordCard key={wordObject.id} english={wordObject.原词} chinese={wordObject.译本译词} />
+          ))}
+          </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/word_card.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const style = {
+    container: {
+        flexGrow: 1,
+        flexBasis: 0,
+        background: 'white',
+        margin: '4px',
+    },
+    text: {
+        color: 'black'
+    }
+}
+
+const WordCard = (props) => {
+    return (
+        <div style={style.container} key={props.key}>
+            <p style={style.text}>原词: {props.english}</p>
+            <p style={style.text}>译本译词: {props.chinese}</p>
+        </div>
+    )
+}
+
+export default WordCard;
