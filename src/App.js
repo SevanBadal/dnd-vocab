34b8e46ed60e1447327314aabfd2df58f4b03aa2
sevan golnazarian <sgolnaza@gmail.com>
2020-09-23 20:50:13 -0400

    1. input field made into component 2. refactor of app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import './App.css';
 import data_csv from './clean_data.csv';
+import SearchInput from './components/search_input';
 import WordCard from './components/word_card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 
 const d3 = require('d3');
-
 
 function App() {
 
@@ -44,27 +44,22 @@ function App() {
     return handleEnglish();
   }, [words, searchTerm]);
 
+  const displayWordCards = () => {
+    return results.map(wordObject => (
+      <WordCard key={wordObject.id} english={wordObject.原词} chinese={wordObject.译本译词} />
+    ))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <h2>DnD 5E 词表卡</h2>
-        <div>
-      
           <a href="https://github.com/SevanBadal/dnd-vocab">  <FontAwesomeIcon icon={faGithub} /></a>
-        </div>
         <div className="Word-container">
-          <input 
-            className="input"
-            type="text" 
-            placeholder="Tiefling" 
-            value={searchTerm}
-            onChange={handleQuery} 
-            />
-          {results.map(wordObject => (
-            <WordCard key={wordObject.id} english={wordObject.原词} chinese={wordObject.译本译词} />
-          ))}
+          <SearchInput class={"input"} value={searchTerm} handleQuery={handleQuery}/>
+          {displayWordCards()}
           </div>
-      </header>
+      </div>
     </div>
   );
 }
